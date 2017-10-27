e4a923cda5fe9d30fddf84d189cc3dcfb3096d04
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2017-10-28 01:37:39 +0530

    Removed config folder

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const config = require('./config/database');
 
 var MongoClient = mongodb.MongoClient;
 
--- config/database.js
+++
@@ -1,8 +0,0 @@
-export MONGOLAB_URI = "mongodb://username:password@ds01316.mlab.com:1316/hemakshis-shorturl"
-
-var url = process.env.MONGOLAB_URI;
-
-module.exports = {
-  database:url,
-  secret:'yougogirl'
-}
