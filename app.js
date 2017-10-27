33696d7710af3bcb59e5a4c83aa8ee86630fd216
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2017-10-28 01:10:26 +0530

    DB changes

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+var MongoClient = mongodb.MongoClient;
+
 // Connect to the DB
-mongoose.connect(config.database);
-let db = mongoose.connection;
-
-// Checking Connection
-db.once('open', function(){
-  console.log('Connected to MongoDB');
-});
-
-// Check for DB Errors
-db.on('error', function(err){
-  console.log(err);
+export MONGOLAB_URI = "mongodb://username:password@ds01316.mlab.com:1316/hemakshis-shorturl"
+var url = process.env.MONGOLAB_URI;
+MongoClient.connect(url, function(err, db){
+  if(err){
+    console.log('Unable to connect to the database');
+  } else {
+    console.log('Connection established to ' + url);
+  }
 });
 
 // Init App
