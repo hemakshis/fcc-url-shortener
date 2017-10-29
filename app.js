b66583073a04377e87e83aa4c18f266c88908c74
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2017-10-29 11:49:39 +0530

    connecting to db

--- app.js
+++ app.js
@@ -46,5 +46,5 @@ app.use('/', api);
 
 // Start Server
 app.listen(port, function(){
-  console.log('Server started on port 3000...');
+  console.log('Server started on port ' + port + '...');
 });
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://hemakshis:yougogirl@ds237445.mlab.com:37445/hemakshis-shorturl'
+  database: 'mongodb://hemakshis:yougogirl@ds237445.mlab.com:37445/hemakshis-shorturl',
   secret: 'nonu'
 }
